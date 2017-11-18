838934a87f8116ca004f73d6f1f37e55942ac7c7
MooItsara <itsara.tim@gmail.com>
2017-11-18 15:39:35 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var itemRouter = require('./routers/item');
+
+var app = express();
+
+var PORT = 5000;
+
+app.set('view engine', 'ejs');
+
+mongoose.connect('mongodb://lansky:vespadark2@ds249025.mlab.com:49025/simpleapi1');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use('/', itemRouter);
+
+app.listen(PORT, function () {
+  console.log('Listening on port ' + PORT);
+});
